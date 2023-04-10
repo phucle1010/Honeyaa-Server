60974b8f09f8f07db1612ab31e80af584f506afb
Duc47 <nvd501150@gmail.com>
2023-04-10 16:22:17 +0700

    connectDB

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
 
 const PORT = 8080;
@@ -9,8 +9,21 @@ app.get('/api', (req, res) => {
         message: "Access API Successfully"
     })
 })
-
-
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'Honeyaa'
+  });
+  
+  // Kết nối cơ sở dữ liệu
+  db.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('MySQL connected');
+  });
+  
 // Get data from /api/user
 // Get data from /api/signin
 // Get data from /api/signup
