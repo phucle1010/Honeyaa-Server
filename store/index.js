032f80058b65d4e1bf15fb08a472ce2987527363
Annoy1010 <lethephuc2002@gmail.com>
2023-07-14 22:20:01 +0700

    config-db

--- store/index.js
+++ store/index.js
@@ -8,11 +8,21 @@ const db = mysql.createPool({
     // database: process.env.DATABASE_NAME,
     // port: '3306',
 
-    host: 'sql10.freesqldatabase.com',
-    user: 'sql10632404',
-    password: 'FbJ5c1mu1s',
-    database: 'sql10632404',
-    port: '3306',
+    // host: 'sql10.freesqldatabase.com',
+    // user: 'sql10632404',
+    // password: 'FbJ5c1mu1s',
+    // database: 'sql10632404',
+    // port: '3306',
+    host: 'gateway01.us-east-1.prod.aws.tidbcloud.com',
+    port: 4000,
+    user: '8fQiJvi8jQBZMD3.root',
+    password: 'VhRD4nDFPlVlT3hs',
+    database: 'honeyaa',
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true,
+    },
+    connectTimeout: 20000,
 });
 
 module.exports = db;
